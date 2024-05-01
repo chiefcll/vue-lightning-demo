4c2ca74afb5424b36a284d598ccdb0cd1064ae8b
Chris Lorenzo <christopher_lorenzo@comcast.com>
2024-05-01 14:57:32 -0400

    add custom fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
+import { importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    importChunkUrl(),
     vue({
       template: {
         compilerOptions: {
